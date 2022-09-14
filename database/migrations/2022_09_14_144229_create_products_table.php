1086fa7ce4be354a434ecc85ccee81c79d490d82
Sahan Dissanayake | Disapamok <sahandizanayake@gmail.com>
2022-09-14 20:21:55 +0530

    Initial commit

---
+++ database/migrations/2022_09_14_144229_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('name', 255);
+            $table->double('price');
+            $table->text('description')->nullable();
+            $table->string('image', 500);
+            $table->unsignedBigInteger('added_by');
+            $table->foreign('added_by')->references('id')->on('users');
+            $table->unsignedBigInteger('updated_by');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
