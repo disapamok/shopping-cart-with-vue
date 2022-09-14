e44cd972a47ce92b78a11891bddf4648ceda92c6
Sahan Dissanayake | Disapamok <sahandizanayake@gmail.com>
2022-09-14 22:21:59 +0530

    Created middlewares to protect routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\IsAdmin::class,
+        'sales_manager' => \App\Http\Middleware\IsSalesManager::class,
+        'operation_manager' => \App\Http\Middleware\IsOperationManager::class
     ];
 }
---
+++ app/Http/Middleware/IsOperationManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Roles;
+use Closure;
+
+class IsOperationManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check() && auth()->user()->role_id == Roles::OPERATION_MANAGER) {
+            return $next($request);
+        } else {
+            abort(403);
+        }
+    }
+}
