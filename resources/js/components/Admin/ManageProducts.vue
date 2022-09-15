e452a24a56fa009299df73fb304945dae5919bce
Sahan Dissanayake | Disapamok <sahandizanayake@gmail.com>
2022-09-16 00:25:11 +0530

    admin - separated the add modal

--- resources/js/components/Admin/ManageProducts.vue
+++ resources/js/components/Admin/ManageProducts.vue
@@ -43,76 +43,21 @@
             </div>
         </div>
 
-        <div class="modal" tabindex="-1" role="dialog" id="addProduct">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Modal title</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label>Product Name</label>
-                            <input type="text" placeholder="Product Name" class="form-control" v-model="addProduct.name"/>
-                            <p v-if="addProductErrors.name != null" class="text-danger">{{addProductErrors.name[0]}}</p>
-                        </div>
-                        <div class="form-group">
-                            <label>Category</label>
-                            <select class="form-control" v-model="addProduct.category">
-                                <option v-for="category in categoriesList" :key="category.id" :value="category.id">{{category.name}}</option>
-                            </select>
-                            <p v-if="addProductErrors.category != null" class="text-danger">{{addProductErrors.category[0]}}</p>
-                        </div>
-                        <div class="form-group">
-                            <label>Product Price</label>
-                            <input type="text" placeholder="Product Price" class="form-control" v-model="addProduct.price"/>
-                            <p v-if="addProductErrors.price != null" class="text-danger">{{addProductErrors.price[0]}}</p>
-                        </div>
-                        <div class="form-group">
-                            <label>Image URL</label>
-                            <input type="text" placeholder="Product Price" class="form-control" v-model="addProduct.imageURL"/>
-                            <p v-if="addProductErrors.imageURL != null" class="text-danger">{{addProductErrors.imageURL[0]}}</p>
-                        </div>
-                        <div class="form-group">
-                            <label>Product Description</label>
-                            <textarea type="text" placeholder="Product Description" class="form-control" rows="4" v-model="addProduct.description"></textarea>
-                            <p v-if="addProductErrors.description != null" class="text-danger">{{addProductErrors.description[0]}}</p>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" @click="createProduct()">Add Product</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        <addModal v-bind:categories="categories"/>
     </div>
 </template>
 <script>
+import addModal from './Modals/CreateProduct.vue';
 export default({
+    name: 'ManageProducts',
+    components:{
+        'addModal' : addModal
+    },
     data() {
         return {
             categoriesList : [],
             productsList : [],
             selectedCategory : null,
-
-            addProduct: {
-                name: null,
-                category: null,
-                price: null,
-                imageURL: null,
-                description: null
-            },
-            addProductErrors:{
-                name: null,
-                category: null,
-                price: null,
-                imageURL: null,
-                description: null
-            }
         }
     },
     props : ['categories','products'],
@@ -120,7 +65,6 @@ export default({
         this.categoriesList = this.categories;
         this.productsList = this.products;
         this.selectedCategory = this.categories[0].id;
-        this.addProduct.category = this.categories[0].id;
     },
     computed:{
         filteredProducts : function (){
@@ -130,19 +74,8 @@ export default({
         }
     },
     methods:{
-        createProduct : function (){
-            axios.post('/products/product',this.addProduct).then((response) => {
+        pushProduct : function(){
 
-            }).catch((error) => {
-                this.addProductErrors = {
-                    name: null,
-                    category: null,
-                    price: null,
-                    imageURL: null,
-                    description: null
-                }
-                this.addProductErrors = error.response.data.errors;
-            });
         }
     }
 })
---
+++ resources/js/components/Admin/Modals/CreateProduct.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="modal" tabindex="-1" role="dialog" id="addProduct">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Product Name</label>
+                        <input type="text" placeholder="Product Name" class="form-control" v-model="addProduct.name"/>
+                        <p v-if="addProductErrors.name != null" class="text-danger">{{addProductErrors.name[0]}}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Category</label>
+                        <select class="form-control" v-model="addProduct.category">
+                            <option v-for="category in categories" :key="category.id" :value="category.id">{{category.name}}</option>
+                        </select>
+                        <p v-if="addProductErrors.category != null" class="text-danger">{{addProductErrors.category[0]}}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Product Price</label>
+                        <input type="text" placeholder="Product Price" class="form-control" v-model="addProduct.price"/>
+                        <p v-if="addProductErrors.price != null" class="text-danger">{{addProductErrors.price[0]}}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Image URL</label>
+                        <input type="text" placeholder="Product Price" class="form-control" v-model="addProduct.imageURL"/>
+                        <p v-if="addProductErrors.imageURL != null" class="text-danger">{{addProductErrors.imageURL[0]}}</p>
+                    </div>
+                    <div class="form-group">
+                        <label>Product Description</label>
+                        <textarea type="text" placeholder="Product Description" class="form-control" rows="4" v-model="addProduct.description"></textarea>
+                        <p v-if="addProductErrors.description != null" class="text-danger">{{addProductErrors.description[0]}}</p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" @click="createProduct()">Add Product</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CreateProduct',
+    data() {
+        return {
+            addProduct: {
+                name: null,
+                category: null,
+                price: null,
+                imageURL: null,
+                description: null
+            },
+            addProductErrors:{
+                name: null,
+                category: null,
+                price: null,
+                imageURL: null,
+                description: null
+            }
+        }
+    },
+    props : ['categories'],
+    methods : {
+        createProduct : function (){
+            axios.post('/products/product',this.addProduct).then((response) => {
+                this.showAlert(response.data.message);
+                $('#addProduct').modal('hide');
+            }).catch((error) => {
+                this.addProductErrors = {
+                    name: null,category: null,price: null,imageURL: null,description: null
+                }
+                this.addProductErrors = error.response.data.errors;
+            });
+        }
+    },
+    mounted (){
+        this.addProduct.category = this.categories[0].id;
+    }
+};
+</script>
