8adf6b608da5156712533939775a2224a2c5d891
Sahan Dissanayake | Disapamok <sahandizanayake@gmail.com>
2022-09-19 11:53:48 +0530

    payments - created database tables and models

---
+++ database/migrations/2022_09_19_060526_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('order_number');
+            $table->double('amount');
+            $table->double('discount')->nullable();
+            $table->double('gross_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
